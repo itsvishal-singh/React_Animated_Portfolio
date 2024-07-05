83f866ad21c743f4168cd309b498d5a49085b91a
Vishal <vishal.200087@gmail.com>
2024-07-05 17:25:08 +0530

    react-animated-portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { FaInstagram } from "react-icons/fa";
-import { CiFacebook } from "react-icons/ci";
+// import { CiFacebook } from "react-icons/ci";
 import { CiLinkedin } from "react-icons/ci";
-import { FaSquareXTwitter } from "react-icons/fa6";
+// import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaGithubSquare } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 
@@ -16,23 +16,35 @@ const Contact = () => {
           data-aos="zoom-in-up"
           data-aos-duration="1000"
         >
-          <a href="https://www.google.com" target="_blank" className="items">
+          <a
+            href="https://www.instagram.com/itsvishal_singh?igsh=MWQ4NWZtNmc5NGkwMg=="
+            target="_blank"
+            className="items"
+          >
             <FaInstagram className="icons" />
           </a>
-          <a href="https://www.google.com" target="_blank" className="items">
+          {/* <a href="https://www.google.com" target="_blank" className="items">
             <CiFacebook className="icons" />
-          </a>
-          <a href="https://www.google.com" target="_blank" className="items">
+          </a> */}
+          <a
+            href="https://www.linkedin.com/in/itsvishalsingh?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+            target="_blank"
+            className="items"
+          >
             <CiLinkedin className="icons" />
           </a>
-          <a href="https://www.google.com" target="_blank" className="items">
+          {/* <a href="https://www.google.com" target="_blank" className="items">
             <FaSquareXTwitter className="icons" />
-          </a>
-          <a href="https://www.github.com" target="_blank" className="items">
+          </a> */}
+          <a
+            href="https://github.com/itsvishal-singh"
+            target="_blank"
+            className="items"
+          >
             <FaGithubSquare className="icons" />
           </a>
           <a
-            href="mailto:webdevmastery@gmail.com"
+            href="mailto:vishal.200087@gmail.com"
             target="_blank"
             className="items"
           >
